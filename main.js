610be81787f261440c4d2bf96444ee2ad190d373
Ryan Paul <ryunpaul@gmail.com>
2016-04-02 16:07:34 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 /* Ryan Paul - 03/31/16
- * Scope chains, closures, and bindign
+ * Scope chains, context, closures, and binding
  */
 
 
@@ -74,8 +74,8 @@ t.run_cranked(crankedEl, ["This is run_cranked!", "11/10"], 4000);
  * First and foermost understand the lexicaly scoped function execution context
  * http://davidshariff.com/blog/javascript-scope-chain-and-closures/
  *
- * Common mistake is to refernce scopes unknowingly which can cause memory
- * leaks. Probably the most misunderstood topic of JS, is execution context
+ * Common mistake is to refernce vlosures unknowingly which can cause memory
+ * leaks. Probably the most misunderstood topic of JS, is execution scope
  * chaining.
  * 
  */
@@ -94,15 +94,25 @@ t.run_cranked(crankedEl, ["This is run_cranked!", "11/10"], 4000);
  * }
  * 
  * ++ Loop Steps ++
- * 1) Create anon function that sets a timeout
- * 2) Bind preserves context, and is immediately called with an argument
- * 3) Create Anon function that calls context method with 1's param
- * 4) Bind preserves context, returning new functor
- * 5) 4's functor is sent to the Event Queue with predefined context
+ * 1) Create anon function with one parameter that sets a timeout
+ * 2) Bind called on function preserving only context, then called with argument
+ * 3) Create another Anon function that calls function with 1's scoped param
+ * 4) Bind preserves context, returning new another functor refferemcomg 3's scope
+ * setTimeout: 4's functor is sent to the Event Queue with predefined context
  * 
- * Scope chain: 4 <- 3 (Method Call) -> 2 -> 1
- * All scopes are referencing each other in some way
- */
+ * run_meh() call stack:
+ * (anonymous function)	@	main.js:25 // setTimeout((function(){ }).bind(this), offset * delay);
+ * Test.run_original	@	main.js:28 // ((function(offset){ }).bind(this))(i);
+ * (anonymous function)	@	main.js:66 // t.run_original( ... );
+ * 
+ * Callback call stack:
+ * 0.2ms 100.0%	(anonymous function)
+ * 0.2ms 100.0%	Test.show main.js:14
+ * 0.1ms 65.07%	insertAdjacentHTML
+ * 
+ * As you can see, two extra closures are created and held in memory. One at run_meh,
+ * and another when the callback is called. No bueno.
+ * */
 
 
 
@@ -120,7 +130,19 @@ t.run_cranked(crankedEl, ["This is run_cranked!", "11/10"], 4000);
  * 2) Bind preserves context and arguments by returning new functor
  * 3) 2's functor is sent to the Event Queue with predefined context and param
  *
- * Scope chain: 2 <- 1 (Method Call)
+ * 
+ * run_meh() call stack:
+ * Test.run_meh	@		main.js:39 // setTimeout((function(item) { ... }).bind(this, queue[i]), delay * i);
+ * (anonymous function)	@	main.js:67 // t.run_meh( ... );
+ *
+ * Callback call stack:
+ * 0.2ms 100.0%	(anonymous function)
+ * 0.2ms 100.0%	Test.show main.js:11
+ * 0.0ms 21.04%	insertAdjacentHTML
+ *
+ * Now only one extra closure is formed when the callback
+ * is called. Not bad.
+ * 
  */
 
 
@@ -137,7 +159,14 @@ t.run_cranked(crankedEl, ["This is run_cranked!", "11/10"], 4000);
  * 1) Bind preserves context and arguments by returning new functor
  * 2) 1's functor is sent to the Event Queue with predefined context and param
  *
- * Scope Chain: 1 (Method Call)
- *
- * Shnazzy
+ * run_cranked() call stack:
+ * Test.run_cranked	@	main.js:52 // setTimeout((function(item) { ... }).bind(this, queue[i]), delay * i);
+ * (anonymous function)	@	main.js:68 // t.run_cranked( ... );
+ * 
+ * Callback call stack:
+ * 0.2ms 100.0%	Test.show main.js:11
+ * 0.1ms 33.00%	insertAdjacentHTML
+ * 
+ * No extra closures are created. Just one single binded functor, sexily packaged
+ * up with context and arguments ready for calling. Life is good.
  */
