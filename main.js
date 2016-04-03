95f734256b74ce412c147044aeaafe8f7bf0b145
Ryan Paul <ryunpaul@gmail.com>
2016-04-02 17:06:56 -0700

    Update main.js

--- main.js
+++ main.js
@@ -101,17 +101,20 @@ t.run_cranked(crankedEl, ["This is run_cranked!", "11/10"], 4000);
  * setTimeout: 4's functor is sent to the Event Queue with predefined context
  * 
  * run_original() call stack:
- * (anonymous function)	@	main.js:25 // setTimeout((function(){ }).bind(this), offset * delay);
- * Test.run_original	@	main.js:28 // ((function(offset){ }).bind(this))(i);
- * (anonymous function)	@	main.js:66 // t.run_original( ... );
+ * (anonymous function)  @ main.js:25 // setTimeout((function(){ }).bind(this), offset * delay);
+ * Test.run_original     @ main.js:28 // ((function(offset){ }).bind(this))(i);
+ * (anonymous function)  @ main.js:66 // t.run_original( ... );
  * 
  * Callback call stack:
- * 0.2ms 100.0%	(anonymous function)
- * 0.2ms 100.0%	Test.show main.js:14
- * 0.1ms 65.07%	insertAdjacentHTML
+ * 0.1ms 65.07%	insertAdjacentHTML    @ main.js:14 // el.insertAdjacentHTML("beforeend", html);
+ * 0.2ms 100.0%	Test.show             @ main.js:14 // el.insertAdjacentHTML("beforeend", html); <?>
+ * 0.2ms 100.0%	(anonymous function)  @ main.js:26 // this.show(el, queue[offset]);
  * 
- * As you can see, two extra closures are created and held in memory. One at run_meh,
- * and another when the callback is called. No bueno.
+ * As you can see, two extra closures are created and held in memory. First is in 
+ * the method call wrapping the setTimeout function (top of method stack) which 
+ * is created and called immediately (bound IIFE). Second is created in the 
+ * setTimeout argument (bottom of callback stack). No bueno.
+ * <?> Not sure why this is line 14 instead of 11, like the others. ¯\_(ツ)_/¯
  * */
 
 
@@ -132,16 +135,17 @@ t.run_cranked(crankedEl, ["This is run_cranked!", "11/10"], 4000);
  *
  * 
  * run_meh() call stack:
- * Test.run_meh	@		main.js:39 // setTimeout((function(item) { ... }).bind(this, queue[i]), delay * i);
- * (anonymous function)	@	main.js:67 // t.run_meh( ... );
+ * Test.run_meh          @ main.js:39 // setTimeout((function(item) { ... }).bind(this, queue[i]), delay * i);
+ * (anonymous function) @ main.js:67 // t.run_meh( ... );
  *
  * Callback call stack:
- * 0.2ms 100.0%	(anonymous function)
- * 0.2ms 100.0%	Test.show main.js:11
- * 0.0ms 21.04%	insertAdjacentHTML
+ * 0.0ms 21.04%	insertAdjacentHTML    @ main.js:14 // el.insertAdjacentHTML("beforeend", html);
+ * 0.2ms 100.0%	Test.show             @ main.js:11 // Test.prototype.show = function(el, data){
+ * 0.2ms 100.0%	(anonymous function)  @ main.js:39 // setTimeout((function(item) {
  *
- * Now only one extra closure is formed when the callback
- * is called. Not bad.
+ * The wrapping closure in the method call is removed (main.js:25 is gone). But we
+ * still have the wrapping closure in the setTimeout argument (bottom of callback
+ * stack).
  * 
  */
 
@@ -160,13 +164,15 @@ t.run_cranked(crankedEl, ["This is run_cranked!", "11/10"], 4000);
  * 2) 1's functor is sent to the Event Queue with predefined context and param
  *
  * run_cranked() call stack:
- * Test.run_cranked	@	main.js:52 // setTimeout((function(item) { ... }).bind(this, queue[i]), delay * i);
- * (anonymous function)	@	main.js:68 // t.run_cranked( ... );
+ * Test.run_cranked      @ main.js:52 // setTimeout((function(item) { ... }).bind(this, queue[i]), delay * i);
+ * (anonymous function)  @ main.js:68 // t.run_cranked( ... );
  * 
  * Callback call stack:
- * 0.2ms 100.0%	Test.show main.js:11
- * 0.1ms 33.00%	insertAdjacentHTML
+ * 0.1ms 33.00%	insertAdjacentHTML  @ main.js:14 // el.insertAdjacentHTML("beforeend", html);
+ * 0.2ms 100.0%	Test.show           @ main.js:11 // Test.prototype.show = function(el, data){
  * 
- * No extra closures are created. Just one single binded functor, sexily packaged
- * up with context and arguments ready for calling. Life is good.
+ * This is beautiful. There is no closure wrappers around anything. Just a single 
+ * object returned from bind() set as the callback in setTimeout. Context, and 
+ * arguments are preserved inside the functor's scope, all packaged up, waiting
+ * to be called. Sexy. As. Fuck.
  */
